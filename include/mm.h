8e16ac9e7bb9e078e4a5f61d5fcdaa1cb18c40ff
coolgw <wegao@suse.com>
2019-06-27 16:14:36 +0800

    Draw memory map

--- include/mm.h
+++ include/mm.h
@@ -12,6 +12,17 @@ struct MEMMAN {
     struct FREEINFO free[MEMMAN_FREES];
 };
 
+struct MEMMAP {
+    int base; //int has 32bit
+    int res1;
+    int length;
+    int res2;
+    int type; // 1 can use
+    int res3;
+    int res4;
+    int res5;
+};
+
 void memman_init(struct MEMMAN *man);
 unsigned int memman_total(struct MEMMAN *man);
 unsigned int memman_alloc(struct MEMMAN *man, unsigned int size);
