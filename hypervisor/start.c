75a8b5603863f4d717be25210f6e1f9b5f39b7df
coolgw <wegao@suse.com>
2019-06-27 16:14:36 +0800

    Draw memory map

--- hypervisor/start.c
+++ hypervisor/start.c
@@ -1,6 +1,7 @@
 #include "common.h"
 #include "fifo8.h"
 #include "serial.h"
+#include "mm.h"
 
 
 void enable_mouse(void);
@@ -146,6 +147,35 @@ void drawing_desktop()
 
 }
 
+void drawing_mem_map()
+{
+    unsigned char *vram = VRAM_ADDR;
+    unsigned short xsize,ysize;
+    xsize=320;
+    ysize=200;
+    char buf[50];
+    unsigned int memtotal;
+    struct MEMMAN *memman = (struct MEMMAN *) (MEMMAN_ADDR);
+    memman_init(memman);
+    //print memory map
+    struct MEMMAP *memmap = (struct MEMMAP *) (MEM_MAP_ADDR);
+    int pos=20;
+    for (; memmap->type !=0; ) {
+        sprintf(buf, "base:%X", memmap->base);
+        putfont8_string(vram,xsize, 8, pos, COL8_FFFFFF,font.Bitmap ,buf);
+        sprintf(buf, "len:%X", memmap->length);
+        putfont8_string(vram,xsize, 150, pos, COL8_FFFFFF,font.Bitmap ,buf);
+        if (memmap->type==1) {
+            sprintf(buf, "%s", "free");
+        } else {
+            sprintf(buf, "%s", "reserve");
+        }
+        putfont8_string(vram,xsize, 270, pos, COL8_FFFFFF,font.Bitmap ,buf);
+        memmap++;
+        pos = pos + 16;
+    }
+}
+
 void kernelstart(char *arg)
 {
 
@@ -170,7 +200,7 @@ void kernelstart(char *arg)
 
 
     drawing_desktop();
-
+    drawing_mem_map();
 
     io_stihlt();
     write_string_serial("Hackweek18\r\n");
--- include/common.h
+++ include/common.h
@@ -43,6 +43,8 @@ extern struct bitmap_font font;
 #define COL8_840084     13
 #define COL8_008484     14
 #define COL8_848484     15
+#define MEM_MAP_ADDR (char *)((0x00098004)-(SYSSEG << 4))
+
 
 
 
